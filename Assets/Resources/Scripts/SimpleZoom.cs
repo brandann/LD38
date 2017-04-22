80f452a473049dc32a96526ddabd81e210ec4928
brandan <foilpudding@gmail.com>
2017-04-22 14:21:07 -0700

    zoom slider to show the current zoom level

--- Assets/Resources/Scripts/SimpleZoom.cs
+++ Assets/Resources/Scripts/SimpleZoom.cs
@@ -1,10 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
+using UnityEngine.UI;
 
 public class SimpleZoom : MonoBehaviour {
 
 	public GameObject player;
+    public Slider zoomUI;
+
+    public const float CameraMax = 20;
+    public const float CameraMin = .1f;
 
 	// Use this for initialization
 	void Start () {
@@ -17,21 +22,30 @@ public class SimpleZoom : MonoBehaviour {
 			ZoomOut();
 		if (Input.GetKeyDown(KeyCode.W))
 			ZoomIn();
+
+        zoomUI.value =this.GetComponent<Camera>().orthographicSize / CameraMax;
 	}
 
 	private void ZoomOut()
 	{
 		var cam = this.GetComponent<Camera>();
-        var s = Mathf.Clamp(cam.orthographicSize * .9f, 0.1f, 20);
+        var s = Mathf.Clamp(cam.orthographicSize * .9f, CameraMin, CameraMax);
         cam.orthographicSize = s;
 		player.SendMessage("scaleme", .9f);
+        UpdateSlider();
 	}
 
 	private void ZoomIn()
 	{
 		var cam = this.GetComponent<Camera>();
-        var s = Mathf.Clamp(cam.orthographicSize * 1.1f, 0.1f, 20);
+        var s = Mathf.Clamp(cam.orthographicSize * 1.1f, CameraMin, CameraMax);
         cam.orthographicSize = s;
         player.SendMessage("scaleme", 1.1f);
+        UpdateSlider();
 	}
+
+    private void UpdateSlider()
+    {
+        zoomUI.value = CameraMax / this.GetComponent<Camera>().orthographicSize;
+    }
 }
