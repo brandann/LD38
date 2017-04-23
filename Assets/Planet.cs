63a41398e2362a57d21a4e1780d66106bb70aa7f
brandan <foilpudding@gmail.com>
2017-04-23 00:00:45 -0700

    Planets now bounce off eachother and the walls of the map.

---
+++ Assets/Planet.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Planet : MonoBehaviour
+{
+	private Rigidbody2D _rigidBody2d;
+	private Vector2 _oldVelocity;
+
+	void Start()
+	{
+		_rigidBody2d = gameObject.GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate()
+	{
+		_oldVelocity = _rigidBody2d.velocity;
+
+	}
+
+	void OnCollisionEnter2D(Collision2D c)
+	{
+		if (c.gameObject.tag.Contains("Player"))
+		{
+
+			var thisscale = this.gameObject.transform.localScale.x;
+			var otherscale = c.gameObject.transform.localScale.x;
+
+			if (thisscale > otherscale)
+			{
+				print("kill player");
+				c.gameObject.SendMessage("kill");
+			}
+		}
+		else if (c.gameObject.tag.Contains("Wall") )
+		{
+			//_rigidBody2d.AddForce( -1f * _rigidBody2d.velocity);
+			//_rigidBody2d.AddForce(c.contacts[0].normal * 2f, ForceMode.Impulse);
+			_rigidBody2d.velocity = Vector2.Reflect(_oldVelocity, c.contacts[0].normal);
+
+		}
+		else if (c.gameObject.tag.Contains("Planet"))
+		{
+			//_rigidBody2d.AddForce(-2.5f * _rigidBody2d.velocity);
+			_rigidBody2d.velocity = Vector2.Reflect(_oldVelocity, c.contacts[0].normal);
+
+		}
+
+	}
+
+	void OnTriggerEnter2D(Collider2D c)
+	{
+		if (c.gameObject.tag.Contains("Player"))
+		{
+			var thisscale = this.gameObject.transform.localScale.x;
+			var otherscale = c.gameObject.transform.localScale.x;
+
+			if (thisscale > otherscale)
+			{
+				print("kill player with trigger");
+				c.SendMessage("kill");
+			}
+			else
+			{
+				print("Absorbed to player");
+				c.gameObject.SendMessage("absorb", this.transform.localScale.x);
+				Destroy(this.gameObject);
+			}
+		}
+
+	}
+}
